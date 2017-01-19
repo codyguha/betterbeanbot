38e534ff0170736484d79d1c79b48408f7072bf6
codyguha <aalo.guha@gmail.com>
2017-01-19 12:56:05 -0800

    setup webhooks

--- app/controllers/conversations.js
+++ app/controllers/conversations.js
@@ -5,12 +5,20 @@ module.exports = function (controller) {
   })
 
   // user said hello
-  controller.hears(['hello'], 'message_received', function (bot, message) {
+  controller.hears(['hello', 'hi'], 'message_received', function (bot, message) {
     bot.reply(message, 'Hey there.')
+  })
+
+  controller.hears(['how are you?', 'how are you'], 'message_received', function (bot, message) {
+    bot.reply(message, 'I`m good thanks for asking')
+  })
+
+  controller.hears(['what can i do here?', 'what can i do here'], 'message_received', function (bot, message) {
+    bot.reply(message, 'I am just an echo bot for now. I will repeat back whatever is said to me.')
   })
 
   // user says anything else
   controller.hears('(.*)', 'message_received', function (bot, message) {
-    bot.reply(message, 'you said ' + message.match[1])
+    bot.reply(message, 'I heard ' + message.match[1])
   })
 }
